e332d9fe98f70b1ed51b99efa0f3ff24b6e76fad
imamandrias <64848404+imamandrias@users.noreply.github.com>
2020-10-07 22:58:36 +0700

    Penambahan Murotal

--- playlistyoutube_app/lib/screen/murotal.dart
+++ playlistyoutube_app/lib/screen/murotal.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:playlistyoutube_app/list_video.dart';
+import 'package:playlistyoutube_app/mydrawer.dart';
 
 class MurotalPage extends StatefulWidget {
   @override
@@ -6,12 +9,81 @@ class MurotalPage extends StatefulWidget {
 }
 
 class _MurotalPageState extends State<MurotalPage> {
+  //Fungsi Dashboard untuk menu Murotal
+  Material myList(IconData iconData, String heading, int warna, String link) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      shadowColor: Color(0x802196F3),
+      borderRadius: BorderRadius.circular(24.0),
+      child: InkWell(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) => ListVideo(
+              title: heading,
+              url: link,
+            ),
+          ));
+        },
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //Tulisan sub judulnya
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        heading,
+                        style: TextStyle(color: new Color(warna), fontSize: 20.0),
+                      ),
+                    ),
+                    //Icon Youtube
+                    Material(
+                      color: Color(warna),
+                      borderRadius: BorderRadius.circular(24.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Icon(
+                          iconData,
+                          color: Colors.white,
+                          size: 30.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: MyDrawer(),
       appBar: AppBar(
         title: Text('Murotal'),
         backgroundColor: Colors.cyan,
+      ),
+      body: StaggeredGridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        children: [
+          myList(Icons.live_tv, 'Syaikh Hani Ar Rifai', 0xffed622b,
+              'https://syaikhhani.herokuapp.com/'),
+        ],
+        staggeredTiles: [
+          StaggeredTile.extent(2, 130.0),
+        ],
       ),
     );
   }
